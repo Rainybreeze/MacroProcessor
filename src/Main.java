7caed4535f0b7c9c13878fa6dea8d72b3d994722
Rainybreeze <60471759+Rainybreeze@users.noreply.github.com>
2020-12-27 15:22:03 +0900

    MDT/MNT 테이블 객체 생성
    MDT와 MNT의 정보를 담고 있는 객체 생성

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,41 @@
+import java.util.Vector;
+
+/*
+MNT 클래스
+이 클래스는 MNT 테이블의 정보를 담고 있음
+ */
+class MNT{
+    //fields
+    int index, mdtInd;	//인덱스, mdt 인덱스
+    String definition;	//문자
+    Vector<String> AAT;
+
+    //constructors
+    public MNT(int idx, String def, int mdtInd, Vector<String> arg) {
+        index = idx;
+        definition = def;
+        this.mdtInd = mdtInd;
+        AAT = arg;
+    }
+}
+
+/*
+MDT 클래스
+이 클래스는 MDT 테이블의 정보를 담고 있음
+ */
+class MDT{
+    //fields
+    int idx;		//인덱스
+    String def;		//인덱스 속 매크로
+
+    //constructors
+    public MDT(int i, String string) {
+        idx = i;
+        def = string;
+    }
+}
+
+// 메인 클래스
 public class Main {
     //fields
     Pass1 pass1;
@@ -9,4 +47,18 @@ public class Main {
     }
 
     //methods
+}
+
+// 파일 열기 담당 클래스
+// 파일 여는건 자바 JFileChooser 사용함
+// 어셈블리 파일의 확장자는 *.s
+class FileOpen{
+    public FileOpen(){
+
+    }
+}
+
+//파일 저장 담당 클래스
+class FileSave{
+    public FileSave(){}
 }
